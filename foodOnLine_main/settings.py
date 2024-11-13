1c4d0f28080de6a197c4a7fe2cbad7d623375580
EATrejo <alonsotrejo1970@hotmail.com>
2024-11-12 19:33:12 -0600

    Forgot password feature

--- foodOnLine_main/settings.py
+++ foodOnLine_main/settings.py
@@ -143,3 +143,12 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
     
 }
+
+
+# Email configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'layenaalonso@gmail.com'
+EMAIL_HOST_PASSWORD = 'cpefqmumhdhwqhrp'
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'foodOnLine Marketplace <layenaalonso@gmail.com>'
