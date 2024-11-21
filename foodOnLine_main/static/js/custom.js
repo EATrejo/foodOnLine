5785974794df609781fffb778f8157d03d702dcf
EATrejo <alonsotrejo1970@hotmail.com>
2024-11-21 08:39:31 -0600

    working with counter fooditems

--- foodOnLine_main/static/js/custom.js
+++ foodOnLine_main/static/js/custom.js
@@ -75,3 +75,33 @@ function onPlaceChanged (){
 
 }
 
+$(document).ready(function(){
+    $('.add_to_cart').on('click', function(e){
+        e.preventDefault();
+        
+        food_id = $(this).attr('data-id');
+        url = $(this).attr('data-url');
+
+        data = {
+            food_id: food_id,
+        }
+
+        $.ajax({
+            type: 'GET',
+            url: url,
+            data: data,
+            success: function(response) {
+                console.log(response.cart_counter['cart_count'])
+                $('#cart_counter').html(response.cart_counter['cart_count']);
+                $('#qty-'+food_id).html(response.qty);
+            }
+        })
+    })
+
+    //Place the cart item quantity on load
+    $('.item_qty').each(function(){
+        var the_id = $(this).attr('id')
+        var qty = $(this).attr('data-qty')
+        $('#'+the_id).html(qty);
+    })
+});
--- marketplace/views.py
+++ marketplace/views.py
@@ -1,5 +1,9 @@
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404, render
 
+from .context_processors import get_cart_counter
+
+from .models import Cart
 from menu.models import Category, FoodItem
 from vendor.models import Vendor
 from django.db.models import Prefetch
@@ -22,10 +26,42 @@ def vendor_detail(request, vendor_slug):
             queryset = FoodItem.objects.filter(is_available=True)
         )
     )
-
+    if request.user.is_authenticated:
+        cart_items = Cart.objects.filter(user=request.user)
+    else:
+        cart_items = None
     context = {
         'vendor': vendor,
         'categories': categories,
+        'cart_items': cart_items,
 
     }
     return render(request, 'marketplace/vendor_detail.html', context)
+
+
+
+def add_to_cart(request, food_id):
+    if request.user.is_authenticated:
+        if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+            # Check if the food item mexist
+            try:
+                fooditem = FoodItem.objects.get(id=food_id)
+                # Check if the user has already added that food item to the cart
+                try:
+                    checkCart = Cart.objects.get(user=request.user, fooditem=fooditem)
+                    # Increase Cart quantity
+                    checkCart.quantity += 1
+                    checkCart.save()
+                    return JsonResponse({'status': 'success', 'message': 'Increased Cart quantity', 'cart_counter': get_cart_counter(request), 'qty': checkCart.quantity})
+                except:
+                    checkCart = Cart.objects.create(user=request.user, fooditem=fooditem, quantity=1,)
+                    return JsonResponse({'status': 'success', 'message': 'Added the food to the Cart', 'cart_counter': get_cart_counter(request), 'qty': checkCart.quantity})
+
+            except:
+                return JsonResponse({'status': 'failed', 'message': 'This food does not exist'})
+        else:
+            return JsonResponse({'status': 'failed', 'message': 'Invalid request'})
+        
+    else:
+        return JsonResponse({'status': 'failed', 'message': 'Please, login to continue'})
+
